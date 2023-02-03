110960b92b8445cb1c2b2603b4d0939cf3742933
Edgar Méndez <mdzredgar@gmail.com>
2023-02-02 22:11:40 -0600

    ResidentInfo avanzado

--- src/components/ResidentCard.jsx
+++ src/components/ResidentCard.jsx
@@ -1,17 +1,27 @@
 import axios from 'axios'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const ResidentCard = ({residentUrl}) => {
-
+  const [residentInfo, setResidentInfo] = useState()
 
   useEffect(() => {
     axios.get(residentUrl)
-    .then((res) => console.log(res.data))
+    .then((res) => setResidentInfo(res.data))
     .catch((err) => console.log(err))
   }, [])
 
   return (
-    <div>ResidentCard</div>
+    <article>
+      <div><img src={residentInfo?.image} alt="" /></div>
+      <section>
+        <h3>{residentInfo?.name}</h3>
+        <ul>
+          <li><span>Specie: </span>{residentInfo?.species}</li>
+          <li><span>Origin: </span>{residentInfo?.origin.name}</li>
+          <li><span>Episodes where appears: </span>{residentInfo?.episode.length}</li>
+        </ul>
+      </section>
+    </article>
   )
 }
 
