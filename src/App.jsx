89776dba47a5d9a577a6e9f74737c9abae392382
Edgar Méndez <mdzredgar@gmail.com>
2023-02-03 19:38:51 -0600

    Finalización del entregable

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,18 @@ import ResidentCard from './components/ResidentCard'
 import {getRandomNumber} from './utils/handleRandom'
 
 function App() {
+  //Estado que almacena la información de la location
   const [location, setLocation] = useState()
+  //Estado que almacena la información del input no controlado
   const [nameLocation, setNameLocation] = useState("")
 
+  //Funcion que se ejecuta en el submit del formulario
   const handleSubmit = (e) => {
     e.preventDefault()
     setNameLocation(e.target.idLocation.value)
   }
 
+  //Efecto que se ejecuta en el primer render y cuando cambia nameLocation
   useEffect(() => {
     const dimension = nameLocation === "" ? getRandomNumber(126) : nameLocation
     const URL =`https://rickandmortyapi.com/api/location/${dimension}`
