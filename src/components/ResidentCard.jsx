f5509dbfc486b71f4e8e1515766217ffe24ead12
Edgar Méndez <mdzredgar@gmail.com>
2023-02-03 23:06:24 -0600

    Falta corregir estilos de ResidentCard.jsx

--- src/components/ResidentCard.jsx
+++ src/components/ResidentCard.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import './styles/ResidentCard.css'
 
 const ResidentCard = ({residentUrl}) => {
   const [residentInfo, setResidentInfo] = useState()
@@ -11,16 +12,21 @@ const ResidentCard = ({residentUrl}) => {
   }, [])
 
   return (
-    <article>
-      <div>
+    <article className='residentCard'>
+      <div className='residentCard__img'>
         <img src={residentInfo?.image} alt="" />
       </div>
-      <section>
-        <h3>{residentInfo?.name}</h3>
-        <ul>
-          <li><span>Specie: </span>{residentInfo?.species}</li>
-          <li><span>Origin: </span>{residentInfo?.origin.name}</li>
-          <li><span>Episodes where appears: </span>{residentInfo?.episode.length}</li>
+      <div className="residentCard__status">
+        <div className={`residentCard__status-circle ${residentInfo?.status}`}></div>
+        <h4 className='residentCard__status-name'>{residentInfo?.status}</h4>
+      </div>
+      <section className='residentCard__info'>
+        <h3 className='residentCard__name'>{residentInfo?.name}</h3>
+        <hr className="residentCard__line" />
+        <ul className='residentCard__list'>
+          <li className='residentCard__item' ><span>Specie: </span>{residentInfo?.species}</li>
+          <li className='residentCard__item'><span>Origin: </span>{residentInfo?.origin.name}</li>
+          <li className='residentCard__item'><span>Episodes where appears: </span>{residentInfo?.episode.length}</li>
         </ul>
       </section>
     </article>
