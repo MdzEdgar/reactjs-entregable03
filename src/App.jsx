55a60b4da0e4074059e7fe3e9121c93952556c70
Edgar Méndez <mdzredgar@gmail.com>
2023-02-03 19:32:57 -0600

    Integración de input no controlado

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,27 @@ import {getRandomNumber} from './utils/handleRandom'
 
 function App() {
   const [location, setLocation] = useState()
+  const [nameLocation, setNameLocation] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setNameLocation(e.target.idLocation.value)
+  }
 
   useEffect(() => {
-    const URL =`https://rickandmortyapi.com/api/location/${getRandomNumber(126)}`
+    const dimension = nameLocation === "" ? getRandomNumber(126) : nameLocation
+    const URL =`https://rickandmortyapi.com/api/location/${dimension}`
     axios.get(URL)
     .then((res) => setLocation(res.data))
     .catch((err) => console.log(err))
-  }, [])
+  }, [nameLocation])
 
   return (
     <div className="App">
-      <h1>Hola mundo</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" id='idLocation' placeholder='type a location id' />
+        <button>Search</button>
+      </form>
       <LocationInfo location={location}/>
       {
         location?.residents.map(residentUrl => <ResidentCard key={residentUrl} residentUrl={residentUrl}/>)
