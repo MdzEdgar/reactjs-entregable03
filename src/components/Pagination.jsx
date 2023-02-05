fa79b0a10c929b6f97f46a184fee9fb7a4938bae
Edgar Méndez <mdzredgar@gmail.com>
2023-02-05 17:16:14 -0600

    Estilos para paginación sencilla añadidos.

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { numbersPage } from '../utils/handlePagination'
+import './styles/Pagination.css'
 
 const Pagination = ({setPage, RESIDENTS_PERPAGE, location}) => {
   return (
-    <ul>
+    <ul className='pagination'>
         {
-          numbersPage(location, RESIDENTS_PERPAGE).map(numberPage => <li onClick={() => setPage(numberPage)} key={numberPage}>{numberPage}</li>)
+          numbersPage(location, RESIDENTS_PERPAGE).map(numberPage => <li  className='pagination__page' onClick={() => setPage(numberPage)} key={numberPage}>{numberPage}</li>)
         }
       </ul>
   )
