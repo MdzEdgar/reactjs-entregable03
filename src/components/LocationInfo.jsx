6aa9248791eccf1587227521eaa4816104273f64
Edgar Méndez <mdzredgar@gmail.com>
2023-02-05 17:38:02 -0600

    Estilos de LocationInfo agregados.

--- src/components/LocationInfo.jsx
+++ src/components/LocationInfo.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import './styles/LocationInfo.css'
 
 const LocationInfo = ({location}) => {
   return (
-    <section>
-        <h2>{location?.name}</h2>
-        <ul>
-          <li><span>Type: </span>{location?.type}</li>
-          <li><span>Dimension: </span>{location?.dimension}</li>
-          <li><span>Population: </span>{location?.residents.length}</li>
+    <section className='locationCard'>
+        <h2 className='locationCard__name'>{location?.name}</h2>
+        <ul className='locationCard__list'>
+          <li className='locationCard__item'><span>Type: </span>{location?.type}</li>
+          <li className='locationCard__item'><span>Dimension: </span>{location?.dimension}</li>
+          <li className='locationCard__item'><span>Population: </span>{location?.residents.length}</li>
         </ul>
       </section>
   )
