7728bb0eae2fb7c2135e323d7d66ae4168dcc4ed
Edgar Méndez <mdzredgar@gmail.com>
2023-02-06 23:45:47 -0600

    Avances en el input controlado

--- src/components/ResidentForm.jsx
+++ src/components/ResidentForm.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import './styles/ResidentForm.css'
 
 const ResidentForm = ({handleSubmit}) => {
+const [locationName, setLocationName] = useState([])
+const [input, setInput] = useState()
+
+const handleInput = (e) => {
+  let evalue = e
+  if(evalue !== "") {
+    const URL = `https://rickandmortyapi.com/api/location/?name=${evalue}`
+  console.log(URL)
+  axios.get(URL)
+  .then((res) => setInput(res.data.results))
+  .catch((err) =>console.log(err))
+  } else {
+    let evalue = ""
+  }
+}
+
+
   return (
-    <form className='residentForm' onSubmit={handleSubmit}>
-        <input className='residentForm__input' type="text" id='idLocation' placeholder='type a location id' />
+    <section>
+      <form className='residentForm'>
+        <input className='residentForm__input' type="text" id='inputLocation' placeholder='Enter name' 
+        onChange={(e) => handleInput(e.target.value)} />
         <button className='residentForm__btn' >Search</button>
+        <select name="resultsLocation" id="resultsLocation">
+        </select>
       </form>
+      <div className="locations">
+        {
+          input && (
+            <ul className='pagination'>
+            {
+              input.map(i => <li  className='locations' key={i.url}>{i.url}</li>)
+            }
+          </ul>
+          )
+        }
+      </div>
+    </section>
   )
 }
 
